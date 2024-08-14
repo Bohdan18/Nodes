2bf88895464bc25df534a96a8f5303243a3270e9
Bohdan18 <bohdankhoma@gmail.com>
2024-08-14 16:19:08 +0300

    Create gaianet_install.sh

---
+++ gaianet_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Оновлення та встановлення необхідних пакетів
+sudo apt update -y
+sudo apt-get update -y
+
+# Завантаження та виконання останньої версії скрипта установки GaiaNet Node
+curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash
+
+# Вибір конфігурації Bash
+source ~/.bashrc
+
+# Ініціалізація GaiaNet з конфігурацією
+gaianet init --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
+
+# Запуск ноди
+gaianet start
+
+# Отримання інформації про ноду (Node ID і Device ID)
+gaianet info
