7d503f730d4a6798e41072feb34d5090f9de38dc
Bohdan18 <bohdankhoma@gmail.com>
2024-09-02 03:37:11 +0300

    Update fractallPKey.sh

--- fractallPKey.sh
+++ fractallPKey.sh
@@ -4,7 +4,7 @@
 cd /root/fractald-0.1.7-x86_64-linux-gnu/bin
 
 # Отримати приватний ключ із файлу та вивести його на екран
-awk -F 'checksum,' '/checksum/ {print "Wallet Private Key:" $2}' /root/.bitcoin/wallets/wallet/MyPK.dat
+awk -F 'checksum,' '/checksum/ {print "Wallet Private Key: " $2}' /root/.bitcoin/wallets/wallet/MyPK.dat
 
 cd
 
